f8db7b8cf247956ee1953e139cb91caabbf387e3
Keegan-van-den-berg <ST10508437@myemeris.edu.za>
2026-04-01 14:09:16 +0200

    Added comments to the code

--- src/main/java/com/mycompany/assignment1/Assignment1.java
+++ src/main/java/com/mycompany/assignment1/Assignment1.java
@@ -12,25 +12,32 @@ import java.util.Scanner;
 public class Assignment1 {
 
     public static void main(String[] args) {
+        //Global variables for value validity
         boolean userValid;
         boolean passwordValid;
         boolean phoneValid;
         
+        //Initializing Name and surname
         String Name = "";
         String Surname = "";
         
-        String NameAndSurname = "";
+        //Initializes name and surname for login message
+        String NameAndSurname = ""; 
         
         Scanner scanner = new Scanner(System.in);
         
         //creates a new login object
         Login account1 = new Login();
         
+        //Prints out the menu
         System.out.println("What would you like to do:"
                 + "\n[1] Create an account"
                 + "\n[2] Login");
+        
+        //Gets the choice the user picked
         int choice = scanner.nextInt();
-            
+        
+        //Clears scanner for the next time it is used
         scanner.nextLine();
         
         switch (choice){
@@ -54,7 +61,7 @@ public class Assignment1 {
                 //Looping for when username is incorrectly formatted
                 if (userValid == false){
                     while (account1.checkUserName(Username) == false){
-                        System.out.println(account1.registerUser(false, true, true));
+                        System.out.println(account1.registerUser(false, true, true)); //Prints error message
                         System.out.println("Enter your username: ");
                         String usernameReattempt = scanner.nextLine();
                         userValid = account1.checkUserName(usernameReattempt);
@@ -69,7 +76,7 @@ public class Assignment1 {
                 //Looping for when password is incorrectly formatted
                 if (passwordValid == false){
                     while (account1.checkPasswordComplexity(Password) == false){
-                        System.out.println(account1.registerUser(true, false, true));
+                        System.out.println(account1.registerUser(true, false, true)); //Prints error message
                         System.out.println("Enter your password: ");
                         String passwordReattempt = scanner.nextLine();
                         passwordValid = account1.checkPasswordComplexity(passwordReattempt);
@@ -84,7 +91,7 @@ public class Assignment1 {
                 //Looping for when phone is incorrectly formatted
                 if (phoneValid == false){
                     while (account1.checkCellPhoneNumber(Phone) == false){
-                        System.out.println(account1.registerUser(true, true, false));
+                        System.out.println(account1.registerUser(true, true, false));//Prints error message
                         System.out.println("Enter your phone number: ");
                         String phoneReattempt = scanner.nextLine();
                         phoneValid = account1.checkCellPhoneNumber(phoneReattempt);
@@ -93,6 +100,7 @@ public class Assignment1 {
         
                 //Registers user
                 if (account1.writeUser() == true){
+                    //Prints message that the user has been registered
                     System.out.println(account1.registerUser(userValid, passwordValid, phoneValid));
                 } else {
                     System.out.println("An error occured, please try again.");
@@ -107,9 +115,11 @@ public class Assignment1 {
                 System.out.println("Enter your password: ");
                 String loginPassword = scanner.nextLine();
                 
+                //True only if the username and password both match
                 boolean match = account1.loginUser(loginUsername, loginPassword);
                 
                 if(match == true){
+                    //Gets name and surname for user after successful login
                     NameAndSurname = account1.getNameAndSurname(loginUsername, loginPassword);
                 }
                 
@@ -118,6 +128,7 @@ public class Assignment1 {
                 break;
                 
             default:
+                //If the user did not pick 1 or 2 in the main menu
                 System.out.println("Please choose either of the given options.");
                 break;
         }
--- src/main/java/com/mycompany/assignment1/Login.java
+++ src/main/java/com/mycompany/assignment1/Login.java
@@ -20,15 +20,20 @@ public class Login {
     private String phoneNumber;
     
     public boolean checkUserName(String Username){
+        //Sets global username to the username recieved from main class
         username = Username;
+        
+        //Initializes username validity boolean
         boolean userValid = false;
+        
         //Checks if username is correctly formatted
-        if (username.length() <= 5 && username.contains("_")){
+        if (username.length() <= 5 && username.contains("_")){ //Checks the length and if it conatians an underscore
             userValid = true;
         } else {
             userValid = false;
         }
         
+        //If username is correctly formatted
         if (userValid == true){
             System.out.println("Username successfully captured!");
         }
@@ -37,45 +42,51 @@ public class Login {
     }
     
     public boolean checkPasswordComplexity(String Password){
+        //sets global password variable to Password variable from main class
         password = Password;
+        
+        //Initializes password validity boolean
         boolean passwordValid = false;
         
         //Checks if password meets correct complexity
         //Check the length
         if (password.length() >= 8){
             passwordValid = true;
+        } else {
+            passwordValid = false;
         }
         
         //checks if password contains uppercase
-        for (int i = 0; i < password.length(); i++){
-            if (Character.isUpperCase(password.charAt(i))){
+        for (int i = 0; i < password.length(); i++){ //Checks every character in password
+            if (Character.isUpperCase(password.charAt(i))){ //Checks if the Character is uppercase
                 passwordValid = true;
-                break;
+                break; //Breaks out of for loop if there is an uppercase
             } else {
                 passwordValid = false;
             }
         }
         
         //Checks if password contains a digit
-        for (int i = 0; i < password.length(); i++){
-            if (Character.isDigit(password.charAt(i))){
+        for (int i = 0; i < password.length(); i++){ //Loops through every character in password
+            if (Character.isDigit(password.charAt(i))){ //Checks if character is a digit
                 passwordValid = true;
-                break;
+                break; //Breaks out of for loop if there is a digit
             } else {
                 passwordValid = false;
             }
         }
         
         //Checks if password contains a special character
-        for (int i = 0; i < password.length(); i++){
-            if (!Character.isLetterOrDigit(password.charAt(i))){
+        for (int i = 0; i < password.length(); i++){ //Loops through every character in password
+            if (!Character.isLetterOrDigit(password.charAt(i))){ // Checks if character is a special character
                 passwordValid = true;
-                break;
+                break; //Breaks out of for loop if special char is found
             } else { 
                 passwordValid = false;
             }
         }
         
+        //Displays a message if all requirements for password has been met
         if (passwordValid == true){
             System.out.println("Password successfully captured!");
         }
@@ -84,14 +95,18 @@ public class Login {
     }
     
     public boolean checkCellPhoneNumber(String Phone){
+        //Sets the global phoneNumber variable to the phone variable recieved from the main class
         phoneNumber = Phone;
+        
+        //Initializes the phone validity boolean
         boolean phoneValid = false;
         
         //Checks if phone number is correctly formatted
-        if (phoneNumber.matches("\\+27\\d{9}")){
+        if (phoneNumber.matches("\\+27\\d{9}")){ //checks if phone number begins with "+27" and is 9 digits
             phoneValid = true;
         }
         
+        //Shows a message if all phone number requirements have been met
         if (phoneValid == true){
             System.out.println("Phone number successfully added!");
         }
@@ -100,6 +115,7 @@ public class Login {
     }
     
     public String registerUser(boolean userValid, boolean passwordValid, boolean phoneValid){
+        //Initializes the message variable
         String message = "";
         
         //Message if username is invalid
@@ -118,11 +134,13 @@ public class Login {
                     + "\nPlease try again.";
         }
         
+        //Message if phone number is invalid
         if(phoneValid == false){
             message = "Cell number is incorrectly formatted or does not have an international code."
                     + "\nPlease correct the number and try again.";
         }
         
+        //Message if all three inputs are valid
         if (userValid == true && passwordValid == true && phoneValid == true){
             message = "Username and password are correctly formatted,"
                     + "\nand the user has successfully been registered.";
@@ -131,6 +149,7 @@ public class Login {
         return(message);
     }
     
+    //Sets the name and surname of the user
     public void setNameAndSurname(String Name, String Surname){
         name = Name;
         surname = Surname;
@@ -138,7 +157,9 @@ public class Login {
     
     //Writes user info to txt file
     public boolean writeUser(){
+        //Initializes the boolean that verifies if the info was written to the txt file
         boolean written = false;
+        
         try{
             //Creates a writer object
             FileWriter writer = new FileWriter("Users.txt", true);
@@ -151,47 +172,59 @@ public class Login {
             writer.write("Phone: " + phoneNumber + "\n");
             writer.write("\n");
             
+            //Closes the txt file
             writer.close();
+            
+            //Sets the written boolean to true
             written = true;
-        }catch(IOException e){
-            e.getStackTrace();
+        }catch(IOException e){ //If the try loop experiences any errors
+            e.getStackTrace(); //returns the error
         }
         
         return(written);
     }
     
     public boolean loginUser(String Username, String Password){
+        //Sets the global username to the username from the main class
         username = Username;
+        
+        //Sets the global password to the password from the main class
         password = Password;
         
-        
+        //Initializes the boolean variables that confirm whether or not the username and password were found in the txt file
         boolean userFound = false;
         boolean passwordFound = false;
         
+        //Initializes the boolean that is only true if both the username and password were found
         boolean match = false;
         
-        String line;
-        //creates a new reader object
+        //Initializes the string variable that the text in the txt file will be written to
+        String line = "";
+        
         try{
+            //creates a new reader object
             BufferedReader reader = new BufferedReader(new FileReader("Users.txt"));
             
-            while((line = reader.readLine()) != null){
-                if(line.contains("Username: ")){
-                    String storedUsername = line.substring(10);
+            while((line = reader.readLine()) != null){ //While the line is not empty
+                if(line.contains("Username: ")){ //checks if the line is for the username
+                    String storedUsername = line.substring(10); //Copies the username on that line
                     
+                    //Reads the next line
                     String nextLine = reader.readLine();
                     
+                    //Checks if the line is not empty and is for the password
                     if(nextLine != null && nextLine.contains("Password: ")){
-                        String storedPassword = nextLine.substring(10);
+                        String storedPassword = nextLine.substring(10); //Copies the password
                         
+                        //Checks if the entered username and password match what is stored in the txt file
                         if(storedUsername.equals(username) && storedPassword.equals(password)){
-                            match = true;
+                            match = true; //Sets the match true if they both match
                             break;
                         }
                     }
                 }
             }
-            
+            //Closes the text file
             reader.close();
         } catch(IOException e){
             e.getStackTrace();
@@ -201,31 +234,41 @@ public class Login {
     }
     
     public String getNameAndSurname(String Username, String Password){
+        //Initializes the name and surname variables
         String nameAndSurname = "";
         String storedName = "";
         String storedSurname = "";
+        
         try{
+            //Creates a new buffered reader object
             BufferedReader reader = new BufferedReader(new FileReader("Users.txt"));
             
+            //initializes the string that the line in the txt file will be written to
             String line = "";
             
+            //Runs while the line is not empty
             while((line = reader.readLine()) != null){
-                if (line.contains("Username: ")){
-                   String checkUsername = line.substring(10);
+                if (line.contains("Username: ")){ //Checks if the line is for the username
+                   String checkUsername = line.substring(10); //Copies the username
                    
+                   //Reads the next line
                    String nextCheckLine = reader.readLine();
+                   //checks if the line is not empty and that it is for the password
                    if (nextCheckLine != null && nextCheckLine.contains("Password: ")){
-                       String checkPassword = nextCheckLine.substring(10);
+                       String checkPassword = nextCheckLine.substring(10); //copies the password
                        
+                       //Checks if the entered username & password match what is in the txt file
                        if(checkUsername.equals(Username) && checkPassword.equals(Password)){
-                           String nameLine = reader.readLine();
+                           String nameLine = reader.readLine(); // copies the next line
                            
-                           if (nameLine != null && nameLine.contains("Name: ")){
-                               storedName = nameLine.substring(6);
+                           //checks if line is not empty and that it is has the name
+                           if (nameLine != null && nameLine.contains("Name: ")){ 
+                               storedName = nameLine.substring(6); // copies the name
                                
-                               String surnameLine = reader.readLine();
+                               String surnameLine = reader.readLine(); //copies the next line
+                               //checks if line is not empty and has the surname
                                if (surnameLine != null && surnameLine.contains("Surname: ")){
-                                   storedSurname = surnameLine.substring(9);
+                                   storedSurname = surnameLine.substring(9);//copies the surname
                                    break;
                                }
                            }
@@ -233,21 +276,25 @@ public class Login {
                    }
                 }
             }
-            
+            //closes the text file
             reader.close();
         } catch (IOException e){
             e.getStackTrace();
         }
         
-        nameAndSurname = storedName + " " + storedSurname;
+        nameAndSurname = storedName + " " + storedSurname; //sets the name and surname string
         return(nameAndSurname);
     }
     
     public String returnLoginStatus(boolean match, String NameAndSurname){
+        //initializes the message string 
         String message = "";
+        //Checks if the username and password match what is in the txt file
         if (match == true){
+            //displays welcome message
             message = "Welcome " + NameAndSurname + ", its great to see you!";
         } else {
+            //shows error message
             message = "Username or password incorrect, please try again.";
         }
         
